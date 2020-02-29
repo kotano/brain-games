61af856abe57f805be17bd49176c2ace0d186d21
kotano <kot4no@gmail.com>
2020-03-01 00:31:34 +0300

    authorization feature

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,22 @@
+import json
+from os.path import dirname, join
+
 import prompt
 
 
+FILENAME = join(dirname(__file__), 'usr', 'user.json')
+
+
 def welcome_user():
-    name = prompt.string("May I have your name? ")
-    print(f'Hello, {name}!')
-    return name
+    try:
+        with open(FILENAME) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = prompt.string("May I have your name? ")
+        print('Hello, {}!'.format(username))
+        with open(FILENAME, 'w') as f:
+            json.dump(username, f)
+        return username
+    else:
+        print('Welcome back, {}!'.format(username))
+        return username
